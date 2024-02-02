47332d1ae653bd507a31bce053b24f78b2a3ecd8
raj-iitr <109147954+raj-iitr@users.noreply.github.com>
2024-02-02 17:25:40 +0530

    Update Login.js

--- mernapp/src/screens/Login.js
+++ mernapp/src/screens/Login.js
@@ -11,7 +11,7 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/login", {
+    const response = await fetch("https://thisisafoodapp.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
